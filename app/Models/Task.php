96512965ab6394db3e8395c9306c06799cac0f1a
zack <javck.zack@gmail.com>
2021-10-07 18:20:33 +0800

    未驗證 API 示範完成

---
+++ app/Models/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title','salary','pic','desc','enabled'];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d m:i:s',
+        'updated_at' => 'datetime:Y-m-d m:i:s',
+    ];
+}
